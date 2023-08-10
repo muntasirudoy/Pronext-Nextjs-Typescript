ea2305b72a7b1d7f494a120f1623b20b7043144d
muntasir <muntasirudoy4@gmaiul.com>
2023-08-10 23:09:24 +0600

    update folder structure , add component folde , update tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,43 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     fontSize: {
-      "54": '54px',
-      "48": '48px',
-      "42": '42px',
-      "36": '36px',
-      "32": '32px',
-      "28": '28px',
-      "24": '24px',
-      "20": '20px',
-      "18": '18px',
-      "16": '16px',
-      "14": '14px',
+      "54": "54px",
+      "48": "48px",
+      "42": "42px",
+      "36": "36px",
+      "32": "32px",
+      "28": "28px",
+      "24": "24px",
+      "20": "20px",
+      "18": "18px",
+      "16": "16px",
+      "14": "14px",
     },
 
-      extend: {
-        fontFamily: {
-          poppins: ['Poppins', 'sans-serif'],
-          Encode: ['Encode Sans Expanded', 'sans-serif']
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        Encode: ["Encode Sans Expanded", "sans-serif"],
+      },
+      colors: {
+        primary: "#E9BF97",
+        secondary: "#fdf8f6",
+        black: {
+          100: "#1C1D21 ",
+          75: "#1C1c21 ",
+          50: "#212025 ",
+          25: "#232228 ",
         },
-        colors: {
-          primary : "#E9BF97",
-          secondary : "#fdf8f6",
-        }
-      }
-  
+      },
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
